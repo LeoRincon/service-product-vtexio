fca6d277aa78fa15d2b485ce139bace6c939fcc3
Sebastian G <sgarcias667@gmail.com>
2023-03-29 12:45:26 -0300

    refactor! : remove unused files

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,16 +1,4 @@
 import { Request, Response } from 'express'
-import RequestsSku from '../service/requestsSku'
-import RequestsPrice from '../service/requestsPrice'
-import RequestsInventory from '../service/requestsInventory'
-import RequestsProduct from '../service/requestsProduct'
-import { validatorCategoryBrand } from '../service/validators'
-
-import { DataInventoryExtended } from '../type'
-
-const requestsProduct = new RequestsProduct()
-const requestsSku = new RequestsSku()
-const requestsPrice = new RequestsPrice()
-const requestsInventory = new RequestsInventory()
 
 // ==================================================
 const postProducts = async (req: any, res: any) => {
